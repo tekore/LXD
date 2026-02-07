d7e5b2a54388cd7436197d0d3c8179f3bd7908d0
tekore <tekore@tuta.com>
2026-02-07 16:31:10 +0100

    updates

--- vms.tf
+++ vms.tf
@@ -1,12 +1,13 @@
 resource "lxd_instance" "instance1" {
-  name  = "instance1"
+  count = 10
+  name  = "instance-${count.index}"
   image = "ubuntu-daily:22.04"
-
+  type = virtual-machine
   config = {
     "boot.autostart" = true
   }
 
   limits = {
-    cpu = 2
+    cpu = 1
   }
 }
