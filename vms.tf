f5f063d3d30c8574477cc74459a3afa5293180dd
tekore <tekore@tuta.com>
2026-02-07 17:04:57 +0100

    updates

--- vms.tf
+++ vms.tf
@@ -1,12 +1,13 @@
-resource "lxd_cached_image" "xenial" {
+resource "lxd_cached_image" "ubuntu2404" {
   source_remote = "ubuntu"
-  source_image  = "xenial/amd64"
+  source_image  = "24.04"
+  type          = "virtual-machine"
 }
 
 resource "lxd_instance" "instance1" {
   count = 30
   name  = "instance-${count.index}"
-  image = lxd_cached_image.xenial.fingerprint
+  image = lxd_cached_image.ubuntu2404.fingerprint
   type = "virtual-machine"
   config = {
     "boot.autostart" = true
@@ -15,5 +16,5 @@ resource "lxd_instance" "instance1" {
     cpu = 2
   }
   ephemeral = false
-  depends_on = [ lxd_cached_image.xenial ]
+  depends_on = [ lxd_cached_image.ubuntu2404 ]
 }
